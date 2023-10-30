9a42481f3591622f4bf464bc6d564aa1954ce724
Dikshit-maan <149257007+Dikshit-maan@users.noreply.github.com>
2023-10-30 12:04:40 +0000

    Data-types

---
+++ 01-basic/data-types.js
@@ -0,0 +1,26 @@
+"use strict" //  trate all js code as a newer version\
+
+// alert(" 3 + 3")  we use node.js not browser - it will work in browser 
+
+console.log( 99 +
+     9 ) // Readibility should be high:
+
+
+console.log("Dikshit Maan")    
+
+let name = ("Dikshit")
+let age = 22
+let isloggedIn = true
+
+// Number => 2 to power 53
+// Bigint 
+// string => ""
+// boolean => true/false
+// Null => standalone valur
+// Undifined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // Object
